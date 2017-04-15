ca71fee4b8cde04f4176f394c4597f999c9bdc82
James <James>
2017-04-15 14:26:12 -0400

    fixed handling of no valid input

--- Driver.java
+++ Driver.java
@@ -1,68 +1,60 @@
+
 import java.util.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 
 public class Driver{
-     
-      
+
+
 	public static void main(String[] args){
-//       markov.addWordsToChain("hello this is my name song");
-//       
-//       int x = 0;
-//       if(x == 0) return;
-      
-		Scanner in = new Scanner(System.in);
-      
-      System.out.println("Enter Song Title: ");
-		
-      String name = in.nextLine();
-		String[] words = name.split(" ");
-		MarkovChain chain = read(new File("Files/DAMN.txt"), words);
-            
-      System.out.println(name + "\n By, Timmy Mac");
-               
-      for(int i=0; i<20; i++){
-         System.out.println(chain.getSentence(10));
-      }
-//       String word1 = "\n";
-//       String suf = "";
-//       
-//       while(suf != "\n"){
-//          Prefix pre = new Prefix(word1,word2);
-//          suf = chain.getWord
-//       }
-      
-	}
 	
+		Scanner in = new Scanner(System.in);
+	
+		System.out.println("Enter Song Title: ");
+		
+		String name = in.nextLine();
+		String[] words = name.split(" ");
+		
+		
+		MarkovChain chain = read(new File("src/hamp/DAMN.txt"), words);
+
+		for(int i=0; i<20; i++){
+			System.out.println(chain.getSentence(10));
+		}
+	
+	}
+
 	public static MarkovChain read(File file, String[] words){
-      
-      MarkovChain markov = new MarkovChain();
-      
+		int cntr = 0;
+		MarkovChain markov = new MarkovChain();
+
 		try{
 			Scanner scan = new Scanner(file);
-			//String curr = scan.next();
-         String curr = "";
+			String curr = "";
 			String prev = "";
 			while(scan.hasNextLine()){
+				cntr++;
 				prev = curr;
 				curr = scan.nextLine();
+				if(cntr % 5 == 0){
+					markov.addWordsToChain(curr);
+				}
 				for(int i = 0; i < words.length; i++){
-					//if(curr.contains(words[i])){
-               if(curr.contains(" " + words[i]) || curr.contains(words[i] + " ")){
+					if(curr.contains(" " + words[i]) || curr.contains(words[i] + " ")){
 						if(!prev.equals(" ") && prev != null)
 							markov.addWordsToChain(prev);
 						markov.addWordsToChain(curr);
 						if(scan.hasNext())
 							markov.addWordsToChain(scan.nextLine());
 						break;
-					}
+					}					
 				}
 			}
-         return markov;
+			return markov;
 		}
 		catch(FileNotFoundException e){
 			System.out.println("file not found");
 		}
-      return markov;
+		return markov;
 	}
 }
